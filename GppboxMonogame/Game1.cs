421c22e400628ce3cfa956755fd076f41a30a164
David Elahee <david.elahee@gmail.com>
2025-02-03 18:29:32 +0100

    add monogame box

---
+++ GppboxMonogame/Game1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Gppbox
+{
+
+    public class Game1 : Game
+    {
+        private Texture2D               _bg;
+        private GraphicsDeviceManager   _graphics;
+        private SpriteBatch             _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            _bg = Content.Load<Texture2D>("bg_stars");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw( _bg, new Rectangle(0,0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), new Color(0xffffffff));
+            _spriteBatch.End();
+        }
+    }
+}
---
+++ GppboxMonogame/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Gppbox.Game1();
+game.Run();
